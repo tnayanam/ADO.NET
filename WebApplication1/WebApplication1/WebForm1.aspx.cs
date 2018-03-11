f71adbb883b4dd6f711181e0c0efec4687e97d67
tnayanam <nayatanuj@gmail.com>
2018-03-11 18:35:45 -0400

    ADO.Net Connect to Database and Display in A webformGrid

---
+++ WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"data source=.\MSSQLSERVER01; database=Sample; integrated security=SSPI");
+            SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            con.Close();
+        }
+    }
+}
