5e4b4cf980088015781f8cc2d2f7084579f05b72
tnayanam <nayatanuj@gmail.com>
2018-03-11 18:55:31 -0400

    Creating connection using using

--- WebApplication1/WebApplication1/WebForm1.aspx.cs
+++ WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -7,13 +7,36 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"data source=.\MSSQLSERVER01; database=Sample; integrated security=SSPI");
-            SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
-            con.Open();
-            SqlDataReader rdr = cmd.ExecuteReader();
-            GridView1.DataSource = rdr;
-            GridView1.DataBind();
-            con.Close();
+            // one type of opening connection and closing it
+            //SqlConnection con = new SqlConnection(@"data source=.\MSSQLSERVER01; database=Sample; integrated security=SSPI");
+            //try
+            //{
+            //    SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
+            //    con.Open();
+            //    SqlDataReader rdr = cmd.ExecuteReader();
+            //    GridView1.DataSource = rdr;
+            //    GridView1.DataBind();
+
+            //}
+            //catch (Exception)
+            //{
+            //    throw;
+            //}
+            //finally
+            //{
+            //    con.Close();
+            //}
+
+            // Other type
+            using (SqlConnection con = new SqlConnection(@"data source=.\MSSQLSERVER01; database=Sample; integrated security=SSPI"))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                GridView1.DataSource = rdr;
+                GridView1.DataBind();
+                con.Close();
+            }
         }
     }
 }
