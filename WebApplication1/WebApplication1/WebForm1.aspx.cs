54b31ae3cefdc1d002abba7e4bab5e8e5fe9c70a
tnayanam <nayatanuj@gmail.com>
2018-03-12 21:59:12 -0400

    SQLExecuteReader

--- WebApplication1/WebApplication1/WebForm1.aspx.cs
+++ WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Data.SqlClient;
 using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebApplication1
 {
@@ -34,7 +34,7 @@ namespace WebApplication1
             {
                 SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
                 con.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
+                SqlDataReader rdr = cmd.ExecuteReader(); // since we are getting more than a single values.
                 GridView1.DataSource = rdr;
                 GridView1.DataBind();
                 con.Close();
@@ -42,3 +42,16 @@ namespace WebApplication1
         }
     }
 }
+
+/*     
+ SQLCommand class is used to prepare SQL Command or StoredProcedure
+ that we want to execute on a SQL Server Database.
+
+    The most commonly used methods of the SQL COmmand class
+
+    1. ExecuteReader: Use when the T-SQL statement returns more than a single value. For example if the query returns rows of data.
+
+    2. ExecuteNonQuery - Use when you want to perform an Insert, Update Or Deete Operation
+    3. ExecuteScalar -  Use when the query returns a single scalar value. for examle, queries that return the totdal number of rowas in a table.
+
+     */
