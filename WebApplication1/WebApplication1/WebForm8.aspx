aadd6ae84c91033ff3a17f340f19c0543a0a69d7
tnayanam <nayatanuj@gmail.com>
2018-03-17 17:47:11 -0400

    Cache in Loading data from DB

---
+++ WebApplication1/WebApplication1/WebForm8.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm8.aspx.cs" Inherits="WebApplication1.WebForm8" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+        <asp:Button ID="btnLoadData" runat="server" Text="Load Data" OnClick="btnLoadData_Click" />
+        <asp:Button ID="btnClearCache" runat="server" Text="Clear Cache" OnClick="btnClearCache_Click" />
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#3366CC" BorderStyle="None" BorderWidth="1px" CellPadding="4">
+            <FooterStyle BackColor="#99CCCC" ForeColor="#003399" />
+            <HeaderStyle BackColor="#003399" Font-Bold="True" ForeColor="#CCCCFF" />
+            <PagerStyle BackColor="#99CCCC" ForeColor="#003399" HorizontalAlign="Left" />
+            <RowStyle BackColor="White" ForeColor="#003399" />
+            <SelectedRowStyle BackColor="#009999" Font-Bold="True" ForeColor="#CCFF99" />
+            <SortedAscendingCellStyle BackColor="#EDF6F6" />
+            <SortedAscendingHeaderStyle BackColor="#0D4AC4" />
+            <SortedDescendingCellStyle BackColor="#D6DFDF" />
+            <SortedDescendingHeaderStyle BackColor="#002876" />
+        </asp:GridView>
+        <asp:Label ID="lblMessage" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/WebForm8.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLoadData_Click(object sender, EventArgs e)
+        {
+            if (Cache["Data"] == null)
+            {
+                string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter("Select * from tblProductInventory", con);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds); // now this FILL is very useful as it manages opening of the connection and then executing the command to get the data and the loading it into the dataset and then closes the connection.
+                    GridView1.DataSource = ds;
+                    GridView1.DataBind();
+                    Cache["Data"] = ds;
+                    lblMessage.Text = "Fresh Data Loaded";
+                }
+            }
+            else
+            {
+                GridView1.DataSource = Cache["Data"];
+                GridView1.DataBind();
+                lblMessage.Text = "Data Loaded from cache";
+            }
+
+        }
+
+        protected void btnClearCache_Click(object sender, EventArgs e)
+        {
+            if (Cache["Data"] != null)
+            {
+                Cache.Remove("Data");
+                lblMessage.Text = "Cache cleared";
+            }
+        }
+    }
+}
