3e89cf709a3d905317bf86b898e9e73311b4d207
tnayanam <nayatanuj@gmail.com>
2018-03-18 23:42:12 -0400

    Transaction ROll Back

---
+++ WebApplication1/WebApplication1/WebForm11.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm11.aspx.cs" Inherits="WebApplication1.WebForm11" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        <asp:Label ID="lblMessage" runat="server"></asp:Label>
+        <asp:Button ID="Button1" runat="server" Text="Transfer amount from A to B" OnClick="Button1_Click" Width="277px" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/WebForm11.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm11 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(str))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Accounts", con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(str))
+            {
+                con.Open();
+                SqlTransaction tran = con.BeginTransaction();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("Update Accounts set Balance = Balance - 10 Where AccountNumber = 'A1' ", con, tran);
+                    cmd.ExecuteNonQuery();
+                    cmd = new SqlCommand("Update Accounts set Balance = Balancee + 10 Where AccountNumber = 'A2' ", con, tran);
+                    cmd.ExecuteNonQuery();
+                    tran.Commit();
+                    lblMessage.Text = "Both table updated";
+                }
+                catch (Exception)
+                {
+                    tran.Rollback();
+                    lblMessage.Text = "Transaction RolledBack";
+                }
+                finally
+                {
+
+
+                }
+
+            }
+        }
+    }
+}
